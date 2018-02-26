7a8bad867c02bc0efd8a9960d9652a736d6276bd
Alex Kavanagh <alex@ajkavanagh.co.uk>
2018-02-26 12:05:25 +0000

    Add script helper to sync the devstack hosts to /etc/hosts
    This script iterates through the devstack hosts on serverstack and adds
    the public IP addresses to the /etc/hosts file so that the user can use
    the host names rather than IP addresses.  Note, it needs concommit
    changes in the ~/.ssh/config files to manage the changing IP addresses
    and usernames.

---
+++ sync-hosts-files.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# This script syncs the 'devstack.*' servers that are running in serverstack, with thier public Ip addresses
+# and the end of the /etc/hosts file.  Note that an existing name is deleted as needed.
+
+_dir="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
+_script=$( basename $0 )
+
+# bring in the common functions
+source common-functions.sh
+
+
+if [[ -z ${DEVSTACK_MODIFY_ETC_HOSTS} ]]; then
+	echo "The env DEVSTACK_MODIFY_ETC_HOSTS isn't set so not syncing the devstack.* names"
+	exit 1
+fi
+
+assert_os_vars_are_set
+
+servers=$(openstack server list -c Name -f value | egrep "^devstack")
+
+# remove all of the devstack entries from the /etc/hosts file
+echo "Deleting devstack entries from /etc/hosts"
+sudo sed -i "/devstack\S*\$/d" /etc/hosts
+echo "Done deleting."
+
+# now add back in each of the hosts.
+for server in ${servers[@]}; do
+	echo -n "Determing IP for server: $server ... "
+	find_floating_ip_for $server
+	if [[ "$?" == "1" ]]; then
+		echo "no public IP"
+	else
+		echo "Adding $floating_ip"
+		echo "${floating_ip}	${server}" | sudo tee -a /etc/hosts
+	fi
+done
